56b187a644cb7db67493f090ca558dad2505bcc6
cfaloon <cfaloon@gmail.com>
2016-12-02 09:16:50 -0500

    Start using the World class.

--- ruby-rogue.rb
+++ ruby-rogue.rb
@@ -1,21 +1,17 @@
 #!/usr/bin/env ruby
 
 # All the magic starts here!
+require_relative 'world.rb'
 
 # the characters that will be used as tiles
 TILES = %w(@ # $ % ~)
 
 
-$the_world = [ ['@', '#', '%', '#', '~'],
-               ['#', '$', '%', '#', '$'],
-               ['%', '#', '%', '#', '#'],
-               ['$', '#', '#', '%', '#'],
-               ['%', '$', '#', '#', '#'] ]
+$the_world = World.new( [ ['@', '#', '%', '#', '~'],
+                          ['#', '$', '%', '#', '$'],
+                          ['%', '#', '%', '#', '#'],
+                          ['$', '#', '#', '%', '#'],
+                          ['%', '$', '#', '#', '#'] ] )
 
-def print_world
-  $the_world.each { |row| puts row.join(' ') }
-end
-
-print_world
-
+puts $the_world.to_s # print the world
 
