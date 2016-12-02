0d836c3c00f93fe40533af3f392a02b417be78d4
cfaloon <cfaloon@gmail.com>
2016-12-02 11:04:38 -0500

    Add minitest and write one test.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,10 @@
 source 'https://rubygems.org' do
+  
+  gem 'rake'
+  
+  # test
+  gem 'minitest'
+  
   # debugging
   gem 'pry'
 end
---
+++ test/world_test.rb
@@ -0,0 +1,12 @@
+require_relative '../world.rb'
+require 'minitest'
+require "minitest/autorun"
+
+class WorldTest < Minitest::Test
+
+  def test_to_s
+    world = World.new( [['@', '#'],
+                        ['$', '~']] )
+    assert_equal world.to_s, "@ #\n$ ~"
+  end
+end
