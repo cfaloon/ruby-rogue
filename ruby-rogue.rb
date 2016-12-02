576e2df3984c5f4cd145fe5ffb3869512568a97e
cfaloon <cfaloon@gmail.com>
2016-12-02 18:28:55 -0500

    Remove literal World creation to prepare for command line arguments.

--- ruby-rogue.rb
+++ ruby-rogue.rb
@@ -3,16 +3,6 @@
 # All the magic starts here!
 require_relative 'lib/world.rb'
 
-# the characters that will be used as tiles
-TILES = %w(@ # $ % ~)
+# TODO: Accept command line arguments for generating the maze.
 
 
-$the_world = World.create_as_given( [ ['@', '#', '%', '#', '~'],
-                                      ['#', '$', '%', '#', '$'],
-                                      ['%', '#', '%', '#', '#'],
-                                      ['$', '#', '#', '%', '#'],
-                                      ['%', '$', '#', '#', '#'] ] )
-
-puts $the_world.to_s # print the world
-
-puts World.new(5, 10).to_s
