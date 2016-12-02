903fae770a73a16a674df0f8015ae1924a331f59
cfaloon <cfaloon@gmail.com>
2016-12-02 11:19:34 -0500

    Add height and width instance methods, and unit test them.

--- lib/world.rb
+++ lib/world.rb
@@ -4,6 +4,14 @@ class World
     @two_dimensional_world = two_dimensional_array
   end
 
+  def height
+    @two_dimensional_world.length
+  end
+  
+  def width
+    @two_dimensional_world.first.length
+  end
+  
   def to_s
     @two_dimensional_world.map { |row| row.join(' ') }.join("\n")
   end
--- test/world_test.rb
+++ test/world_test.rb
@@ -5,8 +5,21 @@ require "minitest/autorun"
 class WorldTest < Minitest::Test
 
   def test_to_s
-    world = World.new( [['@', '#'],
-                        ['$', '~']] )
-    assert_equal world.to_s, "@ #\n$ ~"
+    
+    assert_equal simple_world.to_s, "@ # ~\n$ # #"
+  end
+
+  def test_height
+    assert_equal simple_world.height, 2
+  end
+
+  def test_width
+    assert_equal simple_world.width, 3
+  end
+
+  # helpers
+  def simple_world
+    World.new( [['@', '#', '~'],
+                ['$', '#', '#']] )
   end
 end
