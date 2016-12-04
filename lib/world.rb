7fd1fe682ebde8d879a350e2d8301a1f1abc5e64
cfaloon <cfaloon@gmail.com>
2016-12-04 16:03:10 -0500

    Write the input gathering loops and begin re-writing recursive divide strategy.

--- lib/world.rb
+++ lib/world.rb
@@ -4,9 +4,11 @@ class World
 
   def initialize(given_height, given_width)
     @two_dimensional_world = Array.new(given_height) { Array.new(given_width, '.') }
-    recursive_divide(0, 0, given_height, given_width)
-    set_start_and_end
-    add_loot
+    while true
+      recursive_divide(0, 0, given_height, given_width)
+      set_start_and_end
+      break if add_loot
+    end
   end
   
   def height
@@ -58,20 +60,20 @@ class World
   # i settled on a recursive division algorithm with assistance from the internet.
   # credits visable in README. This isn't 100% true to the algorithm I found
   # because my world has no walls between tiles. Tiles are walls.
-  def recursive_divide(x, y, h, w)
-    return if h <= 3 || w < 3 # our base case to end the recursion
+  def recursive_divide_original(x, y, h, w)
+    return if h <= 2 || w <= 2 # our base case to end the recursion
 
     direction = case
-                when h > w; rand() > w / h.to_f ? :horizontal : :vertical
-                when w > h; rand() > h / w.to_f ? :vertical : :horizontal
-                else rand(1) == 0 ? :vertical : :horizontal
+                when h > w; :horizontal
+                when w > h; :vertical
+                else rand(2) == 0 ? :vertical : :horizontal
                 end
     
     wall_x = direction == :horizontal ? x : x + rand(1 .. w - 2)
-    wall_y = direction == :vertical   ? y : y + rand(1 .. h - 2)
+    wall_y = direction == :horizontal ? y + rand(1 .. h - 2) : y
 
-    hole_x = direction == :horizontal ? wall_x + rand(w - 2) : wall_x
-    hole_y = direction == :vertical   ? wall_y + rand(h - 2) : wall_y
+    hole_x = direction == :horizontal ? wall_x + rand(1 .. w - 1) : wall_x
+    hole_y = direction == :vertical   ? wall_y + rand(1 .. h - 1) : wall_y
 
     length = direction == :horizontal ? w : h
 
@@ -79,6 +81,9 @@ class World
     for step in (0..length - 1)
       draw_x = direction == :horizontal ? wall_x + step : wall_x
       draw_y = direction == :vertical ? wall_y + step : wall_y
+      if @two_dimensional_world[draw_y][draw_x] == '%'
+        debug_fail( {x: x, y: y, wall_x: wall_x, wall_y: wall_y, direction: direction, w: w, h: h})
+      end
       @two_dimensional_world[draw_y][draw_x] = '%'
     end
 
@@ -86,23 +91,30 @@ class World
     @two_dimensional_world[hole_y][hole_x] = '.'
 
     # recurse!
-    nw, nh = direction == :horizontal ? [w, wall_y-y ] : [wall_x -x, h ]
-    recursive_divide(x-1, y-1, nh, nw)
+    
+    nw, nh = direction == :horizontal ? [w , wall_y - y] : [wall_x - x , h]
+    recursive_divide([x-1, 0].max, [y-1, 0].max, nh, nw)
     # on the other side of the wall, too.
     new_x, new_y = direction == :horizontal ? [x, wall_y + 2] : [wall_x + 2, y]
     nw, nh = direction == :horizontal ? [w, y + h - wall_y - 2] : [x + w - wall_x - 2, h]
-    recursive_divide(new_x, new_y, nh, nw)
+g    recursive_divide(new_x, new_y, nh, nw)
   end
 
-  # a simpler method that swaps out some free spaces for loot to be picked up
-  # denoted by a $ and dependent upon the world size.
+  def recursive_divide(x, y, h, w)
+    
+  end
+  
   def add_loot
-    2.upto(height-1) do |depth|
-      next if rand(2) == 0 # an attempt at limiting loot
+   2.upto(height-1) do |depth|
+      next unless rand(3) == 0 # an attempt at limiting loot
       position_x = rand(0..width-1)
       if @two_dimensional_world[depth][position_x] == '.'
         @two_dimensional_world[depth][position_x] = '$'
       end
     end
+  end
+
+  def debug_fail(hash)
+    puts hash
   end
 end
--- ruby-rogue.rb
+++ ruby-rogue.rb
@@ -3,6 +3,50 @@
 # All the magic starts here!
 require_relative 'lib/world.rb'
 
+
+
+def custom_prompt
+  integer_regex = /^\d+$/
+  dimensions = []
+  2.times do |n|
+    height_width = n == 0 ? 'height' : 'wdith'
+    while(true) do
+      puts "Please enter a #{height_width} for your custom dungeon. Minimum dimensions are 4x4."
+      print '> '
+      input = gets.chomp
+      if input =~ integer_regex
+        input = input.to_i
+        if input < 4
+          puts "Minimum dimensions are 4x4. Please try again."
+          print '> '
+        else
+          dimensions << input
+          break
+        end
+        puts 'That does not look like an integer.'
+      end
+    end
+  end
+  puts World.new(*dimensions)
+end
+
 # TODO: Accept command line arguments for generating the maze.
+puts 'Welcome to ruby-rogue, please type (s)mall, (m)edium, (l)arge or (c)ustom to generate a dungeon.'
+print '> '
+while(true) do
+  input = gets.chomp
+
+  # parse the input
+  case input
+  when 's', 'small'
+    puts World.new(6,8); break
+  when 'm', 'medium'
+    puts World.new(12, 16); break
+  when 'l', 'large'
+    puts World.new(24, 32); break
+  when 'c', 'custom'
+    custom_prompt; break
+  end
+end
 
 
