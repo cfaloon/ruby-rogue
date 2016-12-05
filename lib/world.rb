2f0595af40ed2ca8430ba995dc2065d3d4200637
cfaloon <cfaloon@gmail.com>
2016-12-04 20:02:22 -0500

    A really cool working version of my new approach.

--- lib/world.rb
+++ lib/world.rb
@@ -2,8 +2,14 @@ class World
 
   attr_accessor :two_dimensional_world
 
+  EMPTY_TILE = '.'
+  WALL_TILE = '#'
+  LOOT_TILE = '$'
+  ENTRY_TILE = '@'
+  EXIT_TILE = '~'
+  
   def initialize(given_height, given_width)
-    @two_dimensional_world = Array.new(given_height) { Array.new(given_width, '.') }
+    @two_dimensional_world = Array.new(given_height) { Array.new(given_width, EMPTY_TILE) }
     while true
       recursive_divide(0, 0, given_height, given_width)
       set_start_and_end
@@ -44,16 +50,16 @@ class World
   def set_start_and_end
     width_range = (0..width-1)
     start_x, start_y = rand(width_range), 0
-    until(@two_dimensional_world[start_y][start_x] == '.')
+    until(@two_dimensional_world[start_y][start_x] == EMPTY_TILE)
       start_x, start_y = rand(width_range), 0
     end
-    @two_dimensional_world[start_y][start_x] = '@'
+    @two_dimensional_world[start_y][start_x] = ENTRY_TILE
     
     end_x, end_y = rand(width_range), height - 1
-    until(@two_dimensional_world[end_y][end_x] == '.')
+    until(@two_dimensional_world[end_y][end_x] == EMPTY_TILE)
       end_x, end_y = rand(width_range), height - 1
     end
-    @two_dimensional_world[end_y][end_x] = '~'
+    @two_dimensional_world[end_y][end_x] = EXIT_TILE
   end
 
   # after much trial and error with my own (unsatisfying) algorithms,
@@ -101,15 +107,47 @@ g    recursive_divide(new_x, new_y, nh, nw)
   end
 
   def recursive_divide(x, y, h, w)
+    return if h <= 2 || w <= 2 # our base case to end the recursion
+
+    # choose the intersection point
+    intersection_x = x + rand(w)
+    intersection_y = y + rand(h / 2 .. h - 1)
+
+    puts({ x: x, y: y, h: h, w: w, intersection_x: intersection_x, intersection_y: intersection_y})
+    # draw the horizontal wall
+    (x..x + w - 1).each do |wall_x|
+      @two_dimensional_world[intersection_y][wall_x] = WALL_TILE
+    end
+
+    (y..y + h - 1).each do |wall_y|
+      @two_dimensional_world[wall_y][intersection_x] = WALL_TILE
+    end
+
+    @two_dimensional_world[intersection_y][intersection_x] = EMPTY_TILE
     
+    direction = rand(2) == 0 ? :horizontal : :vertical
+
+    if direction == :horizontal
+      @two_dimensional_world[intersection_y][intersection_x-1] = EMPTY_TILE if intersection_x >= 1
+      @two_dimensional_world[intersection_y][intersection_x+1] = EMPTY_TILE if intersection_x < width - 1
+    else
+      @two_dimensional_world[intersection_y-1][intersection_x] = EMPTY_TILE if intersection_y >= 1
+      @two_dimensional_world[intersection_y+1][intersection_x] = EMPTY_TILE if intersection_y < height - 1
+    end
+
+    # recurse in each quadrant
+    # top left quadrant
+    recursive_divide(x, y, intersection_y - y - 1, intersection_x - x - 1)
+    # top right quadrant
+    recursive_divide(intersection_x + 2, y, intersection_y - 1, w - intersection_x - 2)
   end
   
   def add_loot
    2.upto(height-1) do |depth|
       next unless rand(3) == 0 # an attempt at limiting loot
       position_x = rand(0..width-1)
-      if @two_dimensional_world[depth][position_x] == '.'
-        @two_dimensional_world[depth][position_x] = '$'
+      if @two_dimensional_world[depth][position_x] == EMPTY_TILE
+        @two_dimensional_world[depth][position_x] = LOOT_TILE
       end
     end
   end
--- ruby-rogue.rb
+++ ruby-rogue.rb
@@ -6,23 +6,24 @@ require_relative 'lib/world.rb'
 
 
 def custom_prompt
-  integer_regex = /^\d+$/
+  integer_regex = /^-?\d+$/
   dimensions = []
   2.times do |n|
-    height_width = n == 0 ? 'height' : 'wdith'
+    height_width = n == 0 ? 'height' : 'width'
     while(true) do
-      puts "Please enter a #{height_width} for your custom dungeon. Minimum dimensions are 4x4."
+      puts "Please enter a #{height_width} for your custom dungeon. Minimum #{height_width} is 4."
       print '> '
       input = gets.chomp
       if input =~ integer_regex
         input = input.to_i
         if input < 4
-          puts "Minimum dimensions are 4x4. Please try again."
+          puts "Minimum #{height_width} is 4. Please try again."
           print '> '
         else
           dimensions << input
           break
         end
+      else
         puts 'That does not look like an integer.'
       end
     end
@@ -46,6 +47,8 @@ while(true) do
     puts World.new(24, 32); break
   when 'c', 'custom'
     custom_prompt; break
+  else
+    puts "Please choose either (s)mall, (m)edium, (l)arge or (c)ustom to generate a dungeon."
   end
 end
 
